317d9f4c5632e7fcdd425396b7b225dc736bee70
Bernard <cesarzb@protonmail.com>
2022-09-14 10:31:24 +0200

    Added secure password

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Authentication', type: :request do
     describe 'POST /authenticate' do
-        let(:user)  { FactoryBot.create(:user, username: 'User321') }
+        let(:user)  { FactoryBot.create(:user, username: 'User321', password: 'password') }
         let(:token) { AuthenticationTokenService.call(user.id) }
         
         it 'authenticates the client' do
-            post '/api/v1/authenticate', params: { username: user.username, password: 'password'}
+            post '/api/v1/authenticate', params: { username: user.username, password: user.password}
 
             expect(response).to have_http_status(:created)
             expect(response.body).to eq(
@@ -15,7 +15,7 @@ RSpec.describe 'Authentication', type: :request do
         end
         
         it 'returns error when username is missing' do
-            post '/api/v1/authenticate', params: { password: 'password' }
+            post '/api/v1/authenticate', params: { password: user.password }
             
             expect(response).to have_http_status(:unprocessable_entity)
             expect(response.body).to eq(
@@ -24,12 +24,18 @@ RSpec.describe 'Authentication', type: :request do
         end
             
         it 'returns error when password is missing' do
-            post '/api/v1/authenticate', params: { username: 'User321' }
+            post '/api/v1/authenticate', params: { username: user.username }
             
             expect(response).to have_http_status(:unprocessable_entity)
             expect(response.body).to eq(
                 { 'error': 'param is missing or the value is empty: password' }.to_json
-                )
+            )
+        end
+
+        it 'returns error when password is incorrect' do
+            post '/api/v1/authenticate', params: { username: user.username, password: 'incorrect' }
+
+            expect(response).to have_http_status(:unauthorized)
         end
     end
 end
